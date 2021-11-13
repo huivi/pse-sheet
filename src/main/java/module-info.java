1834cbdd66ebe32f6e1602f193959dd83c82c106
Benedict <benedict.bauer.1997@googlemail.com>
2021-11-13 11:51:50 +0100

    b

--- src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-open module exercise {
-    requires transitive de.hamstersimulator.objectsfirst.inspector;
-    requires javafx.graphics;
-    exports de.unistuttgart.iste.sqa.pse.sheet03.presence;
-    exports de.unistuttgart.iste.sqa.pse.sheet03.homework;
-}
